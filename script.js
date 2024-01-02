b3940565f0fa0d7112854ac765cdc0c78525c64e
HwangHyeonmi <49122297+HwangHyeonmi@users.noreply.github.com>
2024-01-02 11:54:15 +0900

    project start

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("hi")
+
+
+let searchIcon = document.querySelector('.searchIcon')
+let searchBox = document.querySelector('.searchBox')
+searchIcon.addEventListener("click",function(){
+    alert("click");
+})
